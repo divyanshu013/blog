ae54f3be98b8db6b43bb3ad619b1be66728ac396
Divyanshu Maithani <div.blackcat@gmail.com>
2020-04-03 18:43:49 +0530

    Update newsletter

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
 			instagram: 'https://instagram.com/divyanshu013',
 			email: 'mailto:div.blackcat@gmail.com',
 			stackoverflow: 'https://stackoverflow.com/users/4952669/divyanshu-maithani',
-			newsletter: 'https://tinyletter.com/divyanshu',
+			newsletter: 'https://divyanshu.substack.com/',
 			goodreads: 'https://www.goodreads.com/user/show/62159316-divyanshu-maithani',
 		},
 	},

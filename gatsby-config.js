5ffc550ed95c89f73a84d33e455cabffa0dd098b
Divyanshu Maithani <div.blackcat@gmail.com>
2020-11-30 21:52:29 +0530

    Remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,15 +99,6 @@ module.exports = {
 				devMode: false,
 			},
 		},
-		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				trackingId: 'UA-54730700-3',
-				head: false,
-				anonymize: true,
-				respectDNT: true,
-			},
-		},
 		'gatsby-plugin-catch-links',
 	],
 };
